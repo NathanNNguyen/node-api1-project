f7228dc6c8f6ee892685356d567d6632abe7e920
Nhan <natenguyen51294@gmail.com>
2020-04-18 20:27:32 -0700

    post completed

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const server = express();
 const db = require('./data/db.js');
 server.use(express.json());
 
-server.get('/', async (req, res) => {
+server.get('/', (req, res) => {
   res.send('Server working')
 });
 
+server.post('/api/users', async (req, res) => {
+  const user = req.body;
+  try {
+    const inserted = await db.insert(user);
+    if (user.name && user.bio) {
+      res.status(201).json(user)
+    } else {
+      res.status(400).json({ message: 'Please provide name and bio for the user' })
+    }
+  }
+  catch (err) {
+    res.status(500).json({ err, message: 'There was an error while saving the user to the database' })
+  }
+});
 
 
 const port = process.env.PORT || 5000;
