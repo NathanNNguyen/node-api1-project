31daa0736a9f46edec2bc58b69f7738243e1597c
Nhan <natenguyen51294@gmail.com>
2020-04-18 20:34:31 -0700

    get req done

--- server.js
+++ server.js
@@ -24,6 +24,31 @@ server.post('/api/users', async (req, res) => {
   }
 });
 
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await db.find();
+    res.status(200).json(users);
+  }
+  catch (err) {
+    res.status(500).json({ err, message: 'The users information could not be retrieved' })
+  }
+});
+
+server.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await db.findById(id);
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'The user with the specified ID does not exist' })
+    }
+  }
+  catch (err) {
+    res.status(500).json({ err, message: 'The user information could not be retrieved' })
+  }
+})
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n Listerning on port ${port}... \n`))
